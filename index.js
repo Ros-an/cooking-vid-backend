64f5a3ba2c34c959b72055ae96d554fa3032fb59
Roshan Kr. Mahato <76640700+Ros-an@users.noreply.github.com>
2021-07-12 06:39:41 +0000

    initial commit

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const express = require('express');
-
+const morgan = require("morgan");
+const cors = require("cors");
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello Express app!')
+  res.send('Hello Express, today I will code backend for cookingvid!')
 });
 
 app.listen(3000, () => {
